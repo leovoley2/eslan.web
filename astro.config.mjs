ea11b2ea5dfcb4c7e62ea0cc703bca83528a8392
leobardo sanabria <leovoley2@gmail.com>
2024-07-02 20:48:43 -0500

    configuracion para netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react"; 
 import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
+  edgeMiddleware: true,
   integrations: [tailwind(), react],
   i18n: {
     defaultLocale: 'es',
