5c9dc4ded1d6bdb2b23057740a61b363848ff789
leobardo sanabria <leovoley2@gmail.com>
2024-07-03 10:02:49 -0500

    hjhj

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react"; 
-import vercel from '@astrojs/vercel/serverless';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-  edgeMiddleware: true,
   integrations: [tailwind(), react],
   i18n: {
     defaultLocale: 'es',
@@ -17,5 +13,8 @@ export default defineConfig({
     routing:{
       prefixDefaultLocale: false
     }
-  }
+  },
+  output: 'server',
+  adapter: netlify(),
+  edgeMiddleware: true,
 });
