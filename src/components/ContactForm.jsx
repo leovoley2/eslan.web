80d78156eebd8a9f65fa516eb9b6b030686ff94d
leobardo sanabria <leovoley2@gmail.com>
2024-08-08 18:21:02 -0500

    nuevo

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,51 +1,54 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from "react";
 import emailjs from '@emailjs/browser';
 
-const ContactUs = () => {
-  const formRef = useRef();
-  const [feedbackMessage, setFeedbackMessage] = useState('');
-  const [feedbackColor, setFeedbackColor] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+const EmailFrom = () => {
 
-  const sendEmail = async (e) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (formRef.current.checkValidity()) {
-      setIsLoading(true);
-      try {
-        const response = await emailjs.sendForm(
-          'service_fayk1hj',
-          'template_5e9osht',
-          formRef.current,
-          'ATNa9hCpUCnemBxjE'
-        );
-        console.log('SUCCESS!', response.text);
-        setFeedbackMessage('¡Mensaje enviado con éxito!');
-        setFeedbackColor('text-green-500');
-        console.log('ok')
-      } catch (error) {
-        console.error('FAILED...', error);
-        setFeedbackMessage(`Error al enviar el mensaje: ${error.text}`);
-        setFeedbackColor('text-red-500');
-        console.log('no se envio')
-      } finally {
-        setIsLoading(false);
-      }
-    } else {
-      setFeedbackMessage('Por favor, completa todos los campos correctamente.');
-      setFeedbackColor('text-red-500');
-    }
-  };
+    // Los servicios de Email js
+    const serviceId = 'service_fayk1hj';
+    const templateId = 'template_5e9osht';
+    const publicKey = 'ATNa9hCpUCnemBxjE';
+
+    // creando un nuevo objeto para los contenedores
+    const templateParams = {
+      user_name: name,
+      user_email: email,
+      to_name: 'web wizard',
+      message: message,
+    };
+
+    // envio de email usando emailjs
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+    .then((response) => {
+      console.log('Email enviado correctamente', response);
+      setName('');
+      setEmail('');
+      setMessage('');
+    })
+    .catch((error) => {
+      console.error('Error al enviar el email', error);
+    });
+
+  }
+
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-orange-300 rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-6 text-center">Contactanos</h2>
-      <form ref={formRef} onSubmit={sendEmail} className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label className="block text-sm font-medium text-gray-700">Nombre
           <input
             type="text"
-            name="user_name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
             placeholder="Escriba su nombre"
             className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
@@ -56,8 +59,9 @@ const ContactUs = () => {
           <label className="block text-sm font-medium text-gray-700">Email
           <input
             type="email"
-            name="user_email"
+           value={email}
             required
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Ejemplo@hotmail.com"
             className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
           />
@@ -67,7 +71,8 @@ const ContactUs = () => {
           <label className="block text-sm font-medium text-gray-700">Mensaje
           <textarea
             placeholder="Escriba su consulta"
-            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             required
             className="mt-1 p-2 w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
           />
@@ -77,13 +82,8 @@ const ContactUs = () => {
           Enviar
         </button>
       </form>
-      {feedbackMessage && (
-        <div className={`mt-4 text-center text-sm ${feedbackColor}`}>
-          {feedbackMessage}
-        </div>
-      )}
     </div>
   );
 
 };
-export default ContactUs;
+export default EmailFrom;
