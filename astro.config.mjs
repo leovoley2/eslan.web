d0a6f95c25f9ab8c97b0419d362e6cb78de7a71b
leobardo sanabria <34109009+leovoley2@users.noreply.github.com>
2025-10-04 17:49:51 -0500

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react"; 
-import netlify from '@astrojs/netlify';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing:{
-      prefixDefaultLocale: false
-    }
-  },
-  output: 'server',
-  adapter: netlify(),
-  edgeMiddleware: true,
+  output: 'static',
+  site: 'https://eslan-web.netlify.app',
+  base: '/',
+  build: {
+    format: 'directory'
+  }
 });
